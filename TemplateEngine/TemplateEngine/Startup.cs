04739f2c1333bcf547d831f3255d87f87cc6bcfc
Tuan Nguyen <tuan@users.noreply.github.com>
2014-12-08 22:10:08 -0800

    Add placeholder owin TemplateEngine app

---
+++ TemplateEngine/TemplateEngine/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(TemplateEngine.Startup))]
+
+namespace TemplateEngine
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                return context.Response.WriteAsync("Hello, world.");
+            });
+        }
+    }
+}
