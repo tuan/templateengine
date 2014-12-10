039155281e840f18cf05c9b67a431bc25db9f6d7
Tuan Nguyen <tuan@users.noreply.github.com>
2014-12-10 07:29:12 -0800

    Add TemplateEngine.Tests project

---
+++ TemplateEngine/TemplateEngine.Tests/ControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemplateEngine.Controllers;
+
+namespace TemplateEngine.Tests
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        private string baseAddress = "http://localhost:9000";
+
+        [TestMethod]
+        public void GetMethodReturnsString()
+        {
+            var renderController = new RenderController();
+            
+            var result = renderController.Get();
+
+            Assert.AreEqual(result, "fake result");
+        }
+    }
+}
